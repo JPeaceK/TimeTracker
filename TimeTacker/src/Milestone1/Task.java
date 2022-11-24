e783c525952fa7f5491b1ecd341f2b51e77d450f
JPeaceK <101364548+JPeaceK@users.noreply.github.com>
2022-11-24 18:40:43 +0100

    TASK DBC

--- TimeTacker/src/Milestone1/Task.java
+++ TimeTacker/src/Milestone1/Task.java
@@ -21,6 +21,10 @@ public class Task extends Activity {
    * Constructor with two parameters: String name and Project father.
    */
   public Task(String name, Project father) {
+    //Pre conditions
+    assert(name != null) : "NAME CAN'T BE NULL";
+    assert(father != null) : "FATHER CAN'T BE NULL";
+
     super(name, father);
     this.active = false;
     this.started = false;
@@ -30,6 +34,8 @@ public class Task extends Activity {
     logger.debug("Task parameter constructor");
     logger.trace("Task " + this.getName() + " child of " + this.getFather().getName());
     logger.trace("Intervals: 0");
+
+    assert(invariant());
   }
 
   /**
@@ -42,84 +48,109 @@ public class Task extends Activity {
     this.intervals = new ArrayList<>();
 
     logger.debug("Task default constructor");
+
+    assert(invariant());
   }
 
   @Override
   public Activity getFather() {
+    assert(invariant());
     return this.father;
   }
 
   @Override
   public void setInitialTime(LocalDateTime name) {
-    assert (invariant()) : "TASK INVARIANT ERROR";
-    //PreCondition
+    assert (invariant());
+
+    //Pre condition
     assert (this.active) : "TASK CAN'T SET INITIALTIME IF IT IS NOT ACTIVE";
     assert (this.started) : "TASK CAN'T SET INITIALTIME IF IT IS NOT STARTED";
     this.initialTime = name;
+
+    //Post condition
+    assert(initialTime == name);
   }
 
   @Override
   public void setTotalTime(long totalTime) {
     this.totalTime = totalTime;
+
+    //Post condition
+    assert(this.totalTime == totalTime) : "SET_TOTAL_TIME MUST SET A VALID TIME";
   }
 
   @Override
   public void setFinalTime(LocalDateTime time) {
     this.finalTime = time;
+
+    //Post condition
+    assert(this.finalTime == time): "SET_FINAL_TIME MUST SET A VALID TIME";
   }
 
   @Override
   public void setFather(Project father) {
     this.father = father;
+  
+    assert(this.father == father) : "SET_FATHER DIDN'T WORK AS EXPECTED";
   }
 
   @Override
   public void setName(String name) {
     this.name = name;
+
+    assert(this.name == name) : "SET_NAME DIDN'T WORK AS EXPECTED";
   }
 
   @Override
   public String getName() {
+    assert(invariant());
     return this.name;
   }
 
   @Override
   public LocalDateTime getFinalTime() {
+    assert(invariant());
     return this.finalTime;
   }
 
   @Override
   public long getTotalTime() {
+    assert(invariant());
     return this.totalTime;
   }
 
   @Override
   public LocalDateTime getInitialTime() {
+    assert(invariant());
     return this.initialTime;
   }
 
   @Override
   public void setFinalAndTotalTime(LocalDateTime finalTime, long seconds) {
-    assert (invariant()) : "TASK INVARIANT ERROR";
+    assert(invariant());
 
-    //PreConditions
-    assert (!this.active) : "CAN'TSET FINAL TIME TO AN ACTIVE TASK";
-    assert (this.started) : "CANT SET FINAL TIME TO A NOT STARTED TASK";
+    //Pre conditions
+    assert (!this.active) : "CAN'T SET FINAL TIME TO AN ACTIVE TASK";
+    assert (this.started) : "CAN'T SET FINAL TIME TO A NOT STARTED TASK";
 
     this.finalTime = finalTime;
     this.totalTime = this.totalTime + seconds;
     this.father.setFinalAndTotalTime(finalTime, seconds);
 
     //PostConditions
-    assert (getTotalTime() > 0.0) : "TOTAL TIME MUST BE > 0";
-    assert (this.intervals != null) : "SET FINAL AND TOTAL TIME SHOULD CREATE AN INTERVAL";
+    assert (this.finalTime == finalTime) : "SET_FINAL_AND_TOTAL_TIME DIDN'T WORK AS EXPECTED WITH FINALTIME";
+    assert (this.totalTime == this.totalTime + seconds) : "SET_FINAL_AND_TOTAL_TIME DIDN'T WORK AS EXPECTED WITH TOTALTIME"
+
     logger.debug("Updating task time");
     logger.debug("Task total time: " + this.getTotalTime());
+
+    assert (invariant());
   }
 
   @Override
   public void start() {
-    assert (invariant()) : "TASK INVARIANT ERROR";
+    assert (invariant());
+
     this.active = true;
     this.started = true;
 
@@ -137,20 +168,21 @@ public class Task extends Activity {
 
     //PostConditions
     assert (this.intervals != null) : "START SHOULD CREATE AN INTERVAL";
-    assert (getInitialTime() != null) : "START SHOULD SET AN INITIAL TIME";
-    assert (getFinalTime() == null) : "START DON'T SET ANY FINAL TIME";
+    assert (this.finalTime == null) : "START DON'T SET ANY FINAL TIME";
+ 
+    assert (invariant());
   }
 
   /**
    * Function that stops the task counter.
    */
   public void stop() {
-    assert (invariant()) : "TASK INVARIANT ERROR";
+    assert (invariant());
+
     //PreConditions
     assert (this.active) : "CAN'T STOP A NOT ACTIVE TASK";
     assert (this.started) : "CAN'T STOP A NOT STARTED TASK";
-    assert (this.intervals != null) : "INTERVALS CAN'T BE NULL TO STOP";
-    assert (getFinalTime() == null) : "FINAL TIME ALREADY SETTED";
+    assert (this.finalTime == null) : "FINAL TIME ALREADY SETTED";
 
     this.active = false;
     this.intervals.get(this.intervals.size() - 1).setActive(false);
@@ -160,10 +192,10 @@ public class Task extends Activity {
     logger.debug("Task " + this.getName() + " stopped");
     logger.debug("Intervals: " + this.getIntervals().size());
 
-    //PostConditions
-    assert (!this.active) : "STOP SHOULL SET TASK TO NOT ACTIVE";
-    assert (getFinalTime() != null) : "STOP FINALTIME SET ERROR";
-    assert (getTotalTime() > 0.0) : "STOP TOTALTIME SET ERROR";
+    //PreConditions
+    assert (this.active) : "CAN'T STOP A NOT ACTIVE TASK";
+    assert (this.started) : "CAN'T STOP A NOT STARTED TASK";
+    assert (this.finalTime == null) : "FINAL TIME ALREADY SETTED";
   }
 
   @Override
@@ -181,10 +213,12 @@ public class Task extends Activity {
 
   public void setActive(boolean active) {
     this.active = active;
+        assert(this.active == active) : "SET_ACTIVE DIDN'T WORK AS EXPECTED";
   }
 
   public void setIntervals(ArrayList<Interval> intervals) {
     this.intervals = intervals;
+        assert(this.intervals == intervals) : "SET_INTERVALS DIDN'T WORK AS EXPECTED";
   }
 
   @Override
@@ -200,10 +234,15 @@ public class Task extends Activity {
 
   public ArrayList<String> getTags() {
     return this.tags;
+        assert(invariant());
   }
 
   private boolean invariant() {
-    Activity father = getFather(); //All tasks have a parent.
-    return (father != null);
+    assert (father != null) : "TASK MUST HAVE A FATHER";
+    assert (name != null) : "TASK MUST HAVE A NAME";
+    assert (initialTime != null) : "TASK MUST HAVE AN INITIAL TIME";
+    assert (tags != null) : "TASK MUST HAVE A TAG";
+
+    return true;
   }
 }
